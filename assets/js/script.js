008155105fc49727dd4ea5ed32e1acfc945b3a8e
leof18 <lfriedrich.ieu2019@student.ie.edu>
2021-11-11 11:33:46 +0100

    email

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,5 +85,12 @@ $(function() {
                 return false;
             }
     });
+
+    function sendMail() {
+        var name = $('#contact #name').val();
+        var email = $('#contact #email').val();
+        var message = $('#contact textarea').val();
+        window.location.href = 'mailto:leonhardfriedrich@gmail.com?subject=Reaching out - ' + name + ' (' + email + ')' + '&body=' + message;
+    };
     
 });
