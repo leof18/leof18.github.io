2cd8aa241a488f3fcf1e347db9550ef48f673ef6
leof18 <lfriedrich.ieu2019@student.ie.edu>
2021-11-11 11:56:56 +0100

    email

--- assets/js/emailScript.js
+++
@@ -1,6 +0,0 @@
-function sendMail() {
-    var name = $('#contact #name').val();
-    var email = $('#contact #email').val();
-    var message = $('#contact textarea').val();
-    window.location.href = 'mailto:leonhardfriedrich@gmail.com?subject=Reaching out - ' + name + ' (' + email + ')' + '&body=' + message;
-};
